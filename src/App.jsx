59e041c6d9dc8d142b022318625dc50982b15232
Phosphorus <fernando.n.p.d.moscu@gmail.com>
2022-08-02 03:17:22 -0300

    fix: change some styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,29 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import "./css/Navbar.css"
 import "./css/index.css"
 import { NavBar } from "./components/Navbar.jsx";
-import {CardsInfo} from "./components/CardsInfo.jsx";
+import { CardsInfo } from "./components/CardsInfo.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBarsvg from "./img/NavBar.svg";
+import { ReactComponent as LogoCoders21 } from "./img/logo.svg";
 
-export function App(){
-    return <div>
-        <header>
-            <h1 className='Title'>Coders 21</h1>
-        </header>
-        <div>
-            <NavBar/>
-            <img className="NavSvg" src={NavBarsvg} alt="pepe"/>
+export function App() {
+    return <>
+        <div style={{ position: "fixed", display: "flex", height: "100%", alignItems: "center" }}>
+            <NavBar />
+            <img className="NavSvg" src={NavBarsvg} alt="pepe" />
         </div>
-        <main>
-            <CardsInfo/>
-        </main>
-    </div>;
+        <div>
+            <header>
+                <h1 className='Title'>Coders 21</h1>
+            </header>
+
+            <main className='coders21-container'>
+                <CardsInfo />
+            </main>
+        </div>
+        <div style={{ position: "fixed", bottom: "0", right: "0", marginRight: 20, marginBottom: 10}}>
+            {/* <img src={LogoCoders21}  alt="logo" /> */}
+            <LogoCoders21 fontSize={"8rem"} />
+        </div>
+    </>;
 }
--- src/components/CardsInfo.jsx
+++ src/components/CardsInfo.jsx
@@ -1,43 +1,51 @@
 import "../css/Navbar.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-export function CardsInfo(){
-    return(
-        <div>
-            <div>
-                <div class="card mb-3">       
-                    <img className="card-img-top" src="https://culturacientifica.com/app/uploads/2016/07/universdad.jpg" alt="Card image cap"/>            
-                    <div className="card-body">
-                        <h5 className="card-title">Estas buscando contenido sobre las materias?</h5>
-                        <p className="card-text">Conoce nuestro Drive donde tenemos los
-                            modulos y materiales extras de las materias!</p>
-                        <p className="card-text"><small class="text-muted">Ultima actualización 1/8/22</small></p>
-                        <a className="btn btn-primary" href="#">Drive</a>
-                    </div>
+export function CardsInfo() {
+    return (<>
+        <div className="card mb-3">
+            <img className="card-img-top" src="https://culturacientifica.com/app/uploads/2016/07/universdad.jpg" alt="Card image cap" />
+            <div className="card-body" >
+                <div>
+                    <h5 className="card-title">Estas buscando contenido sobre las materias?</h5>
+                    <p className="card-text">Conoce nuestro Drive donde tenemos los
+                        modulos y materiales extras de las materias!
+                    </p>
                 </div>
-                <div class="card mb-3">
-                    <img className="card-img-top" src="https://culturacientifica.com/app/uploads/2016/07/universdad.jpg" alt="Card image cap"/>
-                    <div className="card-body">
-                        <h5 className="card-title">¿Querés saber cómo vas con el plan de estudio?</h5>
-                        <p className="card-text">Tenemos una página en donde podes ver las
-correlatividades y el plan de estudio!</p>
-                        <p className="card-text"><small class="text-muted">Ultima actualización 1/8/22</small></p>
-                        <a className="btn btn-primary" href="#">Plan de Estudio</a>
-                    </div>
-                </div>
-            </div>
-            <div>
-                <div class="card mb-3">
-                    <img className="card-img-top" src="https://culturacientifica.com/app/uploads/2016/07/universdad.jpg" alt="Card image cap"/>
-                    <div className="card-body">
-                        <h5 className="card-title">Tenemos grupos de WhatsApp y Telegram.</h5>
-                        <p className="card-text">Puedes buscar tus grupos e información 
-extra a traves de estos medios.</p>
-                        <p className="card-text"><small class="text-muted">Ultima actualización 1/8/22</small></p>
-                        <a className="btn btn-primary" href="#">Grupos</a>
-                    </div>
+                <div>
+                    {/* <p className="card-text"><small className="text-muted">Ultima actualización 1/8/22</small></p> */}
+                    <a className="btn btn-primary" href="#">Drive</a>
                 </div>
             </div>
         </div>
+        <div className="card mb-3">
+            <img className="card-img-top" src="https://img2.freepng.es/20180327/aqw/kisspng-emoji-happiness-smiley-sticker-applause-5aba6baacf1134.5744076115221666988482.jpg" alt="Card image cap" />
+            <div className="card-body">
+                <div>
+                    <h5 className="card-title">¿Querés saber cómo vas con el plan de estudio?</h5>
+                    <p className="card-text">Tenemos una página en donde podes ver las
+                        correlatividades y el plan de estudio!</p>
+                    <p className="card-text"><small className="text-muted">Ultima actualización 1/8/22</small></p>
+                </div>
+                <div>
+                    <a className="btn btn-primary" href="#">Plan de Estudio</a>
+                </div>
+            </div>
+        </div>
+        <div className="card mb-3">
+            <img className="card-img-top" src="https://culturacientifica.com/app/uploads/2016/07/universdad.jpg" alt="Card image cap" />
+            <div className="card-body">
+                <div>
+                    <h5 className="card-title">Tenemos grupos de WhatsApp y Telegram.</h5>
+                    <p className="card-text">Puedes buscar tus grupos e información
+                        extra a traves de estos medios.</p>
+                <p className="card-text"><small className="text-muted">Ultima actualización 1/8/22</small></p>
+                </div>
+                <div>
+                    <a className="btn btn-primary" href="#">Grupos</a>
+                </div>
+            </div>
+        </div>
+    </>
     );
 }
